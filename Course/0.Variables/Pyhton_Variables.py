faa7d283776fade436932da7ac2d402e6cdee8ac
Fernando Almeida <fpsalmeida92@gmail.com>
2023-09-20 20:39:02 +0100

    Course initial

---
+++ Course/0.Variables/Pyhton_Variables.py
@@ -0,0 +1,43 @@
+diogo= True
+
+if diogo == True:
+    print("Bom dia Diogo")
+    print("Este trecho é executado")
+    print("Se o diogo for verdadeiro")
+
+nome= input("Introduza o seu nome\n")
+
+
+print("Nome inserido ->", nome)
+
+if nome == "Fernando":
+    print(" Bom dia Fernando")
+else:
+    print(" Bom dia alguem")
+
+
+idade= int(input("Introduza a seu idade\n"))
+
+if idade >= 18:
+    print(" Adulto")
+else:
+    print("Menor")
+
+nome1= "Diogo"
+idade1= 20
+solteiro = False
+
+if solteiro:
+    print("Solteiro")
+else:
+    print("Nao Solteiro")
+
+
+#def BomDia(nome3):
+#    print("Olá bom dia", nome3)
+
+#   BomDia("Joao")
+#    BomDia("Ana")
+#    BomDia("Tomas")
+
+
